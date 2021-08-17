ae4508238cd66c7e0a16c2a614624d9fc0bbbbd2
alalaulita <anatorlaschi@gmail.com>
2021-08-17 17:40:28 -0300

    Subo clase 4

--- clase4.js
+++ clase4.js
@@ -1,26 +1,40 @@
-let temperatura = 25;
-let lluvia = false;
-let saldoSube = 300;
-let valorBoleto = 22;
-let nombreRecepcionista = "Anastasia";
-let horaActual = 8;
-let horaIngresoTrabajo = 9;
-let horaFinTrabajo = 21;
-let cantidadCasosResueltos = 2;
-let nombreJefe = "Ricardo";
+//Como hacer una torta//
+let nombre = "lemon pie";
+let ingrediente1= "huevos";
+let ingrediente2="leche";
+let ingrediente3="limon";
+let ingrediente4="harina";
+let cant_ingrediente1=2;
+let cant_ingrediente2=500;
+let cant_ingrediente3=3;
+let cant_ingrediente4=500;
+let temperatura = 180;
 
-console.log(
-  `Hoy cuando salí de mi casa hacían ${temperatura} grados, pero llovia? ${lluvia}. Me quedaban ${saldoSube} en la tarjeta. Me recibio en la oficina muy simpatica ${nombreRecepcionista}.`
-);
+console.log (nombre, ingrediente1, ingrediente2, ingrediente3, ingrediente4, cant_ingrediente1, cant_ingrediente2, cant_ingrediente3, cant_ingrediente4, temperatura)
+console.log (`Receta para hacer un ${nombre}, ingredientes ${cant_ingrediente1} ${ingrediente1}, ${cant_ingrediente2} ${ingrediente2}, ${cant_ingrediente3} ${ingrediente3}`)
 
-let cajonRemeras = "pantalones"
-let cajonPantalones = "remeras"
+//hacer un cafe//
+let ingrediente5= "cafe";
+let cant_tazas_cafe= 3
+let temperatura2= 80;
+let ingrediente6="agua";
+let cant_agua= 600
+let ingrediente7="azucar"
+let cant_azucar=2;
+let frio= false;
 
-function intercambiarCajones(){
-        let cajonRemeras = "remeras"
-        let cajonPantalones = "pantalones"    
-console.log (`En mi cajon de remeras tengo ${cajonRemeras} y en el de pantalones tengo ${cajonPantalones}`)
+console.log(`Receta para hacer un ${ingrediente5}: para realizar ${cant_tazas_cafe} tazas de ${ingrediente5} se debe utilizar ${ingrediente5}, ${cant_agua}ml de ${ingrediente6} a ${temperatura2} de temperatura, ${cant_azucar} cucharadas de  ${ingrediente7}`)
+
+//Ejercicio pantalones//
+let cajon_pantalon="remera";
+let cajon_remera="pantalon";
+
+let cajon_auxiliar;
+function intercambio() {
+
+cajon_auxiliar= cajon_remera;
+cajon_remera=cajon_pantalon;
+cajon_pantalon = cajon_auxiliar;
 }
-console.log(cajonRemeras)
-console.log(cajonPantalones)
-intercambiarCajones()
+intercambio()
+console.log (`En el cajon de las remeras hay ${cajon_remera} y en el cajon de los pantalones hay ${cajon_pantalon}`)
