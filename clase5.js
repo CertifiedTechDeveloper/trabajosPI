4b165da2820e3ac6282564c883d8ad758be4839c
alalaulita <anatorlaschi@gmail.com>
2021-08-18 21:58:56 -0300

    clase 5

---
+++ clase5.js
@@ -0,0 +1,40 @@
+function test2 (x,y){
+    return x *2
+
+}
+console.log (test2(10))
+
+function pulgadas(x) { 
+    return x*2.54
+}
+console.log (pulgadas (44))
+
+function pepito (a){
+    return "http://www."+ a +".com"
+}
+console.log (pepito("ana"))
+
+function mayuscula (x){
+    return x.toUpperCase ()
+}
+console.log (mayuscula ("augusto"))
+
+function detectar (x){
+    return typeof x
+}
+
+console.log (detectar (true))
+
+function radio (x){ 
+    return 2*x* Math.PI
+}
+
+console.log (radio (25))
+
+
+
+
+
+
+
+
